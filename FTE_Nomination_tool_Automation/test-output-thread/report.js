90cc50725de379f2354b0a6146a578c7e51a5a23
91729 <91729@DESKTOP-7L3CT2G>
2021-04-27 16:22:17 +0530

    BDD Framework - FTE Tool

---
+++ FTE_Nomination_tool_Automation/test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "51d1573d-8fed-4b6b-bd41-f2a039a2159c",
+    "feature": "Login page feature",
+    "scenario": "Login with Valid credentials",
+    "start": 1619184448532,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1619184448648,
+    "className": "undefined"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
